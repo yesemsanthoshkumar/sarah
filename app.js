67ed1c0a02ecf38322e672d16d65260f38e35b86
YESEMSANTHOSHKUMAR <yesemsanthoshkumar@gmail.com>
2016-05-16 00:47:18 +0530

    wrote webhook

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function landing(req, res) {
+    res.send("Hey sweet heart <3")
+})
+
+app.listen(3000, function listening() {
+    console.log("App listening on port 3000")
+})
+
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'validate_bot_sarah') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
