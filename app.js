23fbc493ac0c16d24e5786ea341f0401ebe603ce
YESEMSANTHOSHKUMAR <yesemsanthoshkumar@gmail.com>
2016-05-16 01:39:06 +0530

    resolved port number in app.js

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ app.get('/', function landing(req, res) {
     res.send("Hey sweet heart <3")
 })
 
-app.listen(3000, function listening() {
-    console.log("App listening on port 3000")
-})
-
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === 'validate_bot_sarah') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
+})
+
+app.listen(process.env.PORT || 3000, function listening() {
+    console.log("App listening on port 3000")
 })
