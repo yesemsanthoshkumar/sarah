fd236629170eb2eabab6d14ed76dd49eb1848648
YESEMSANTHOSHKUMAR <yesemsanthoshkumar@gmail.com>
2016-05-16 02:12:54 +0530

    function reply_user created

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
 
+//Landing page of the app
 app.get('/', function landing(req, res) {
     res.send("Hey sweet heart <3")
 })
 
+//Webhook of the app
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === 'validate_bot_sarah') {
     res.send(req.query['hub.challenge']);
@@ -12,6 +14,21 @@ app.get('/webhook/', function (req, res) {
   res.send('Error, wrong validation token');
 })
 
+//Reply to user messages
+app.post('/webhook/', function reply_user(req, res) {
+    var events = req.body.entry[0].messaging
+    for(i = 0; i < events.length; i++)
+    {
+        var event = events[i]
+        if(event.message && event.message.text)
+        {
+            sendMessage(event.sender.if, {text: "Echo: " + event.message.text})
+        }
+    }
+    res.sendStatus(200)
+})
+
+//Port number listening
 app.listen(process.env.PORT || 3000, function listening() {
     console.log("App listening on port 3000")
 })
